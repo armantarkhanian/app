2915a3d9783d6398c7c15064bdcc464511bd47c8
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-03-08 19:34:27 +0300

    sldkfj

--- internal/pkg/handlers/handlers.go
+++ internal/pkg/handlers/handlers.go
@@ -3,7 +3,6 @@ package handlers
 
 import (
 	"app/internal/pkg/server"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +12,6 @@ func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
-func friends(c *gin.Context) {
-	b := 0
-	var id int = 15 / b
-	fmt.Println(id)
-	c.HTML(http.StatusOK, "index.html", nil)
-}
-
 func Init() {
 	server.Router.GET("/", index)
-	server.Router.GET("/friends", friends)
 }
