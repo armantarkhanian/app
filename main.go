035422023004ea8fcccd204dbb00fea1427b8f99
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-05-08 00:49:09 +0300

    sdflj

--- main.go
+++ main.go
@@ -17,29 +17,10 @@ func main() {
 	router := gin.New()
 
 	router.LoadHTMLFiles("index.html")
-	router.Static("/static", "./web/static/")
 	router.StaticFile("/index.js", "./index.js")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", nil)
-	})
-
-	type privateChannel struct {
-		UserID string `json:"client"`
-		Channels []string `json:"channels"`
-	}
-	router.POST("/centrifuge/subscribe", func(c *gin.Context) {
-		var e privateChannel
-		if err := c.ShouldBindJSON(&e); err != nil {
-			c.JSON(200, gin.H{
-				"channels": []string{},
-			})
-			return
-		}
-		fmt.Println(e)		
-		c.JSON(200, gin.H{
-			"channels": []string{},
-		})
 	})
 
 	if err := router.Run(":8080"); err != nil {
